103e33f138ee172c7465c4906e8bba6b206cd56b
Rongfei Pan <rongfei@kth.se>
2024-03-09 21:05:36 +0800

    update before tilestate

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileState : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        public Color backgroundColor;
+        public Color textColor;
+        public int number;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
