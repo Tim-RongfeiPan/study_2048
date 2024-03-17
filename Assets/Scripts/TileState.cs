59899a46de598ac15c713fa19c7c13e2e154e5e1
Rongfei Pan <rongfei@kth.se>
2024-03-17 12:40:34 +0800

    update fonts & tilestate.c

--- Assets/Scripts/TileState.cs
+++ Assets/Scripts/TileState.cs
@@ -1,21 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Tile State")]
 public class TileState : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        public Color backgroundColor;
-        public Color textColor;
-        public int number;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Color backgroundColor; // 背景颜色
+    public Color textColor; // 文字颜色
+    public int number; // 数字
 }
